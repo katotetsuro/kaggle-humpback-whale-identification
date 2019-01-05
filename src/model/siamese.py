c4bb1fbb4dce4be59d0b66c469a34beba4e2ccaa
KatoTetsuro <katotetsuro@team-lab.com>
2019-01-05 09:44:57 +0900

    一端normalizeなしにしてみる・・？

--- src/model/siamese.py
+++ src/model/siamese.py
@@ -7,13 +7,13 @@ class FeatureExtractor(nn.Module):
     def __init__(self, feature_dim=500):
         super().__init__()
         self.net = GapResnet(n_class=feature_dim)
-        self.fc = nn.Linear(feature_dim, 10)
+        self.fc = nn.Linear(feature_dim, 64)
 
     def forward(self, x):
         h = self.net(x)
         h = self.fc(h)
-        l = (h**2).sum(dim=1).sqrt().reshape(-1, 1)
-        h = h / l
+#        l = (h**2).sum(dim=1).sqrt().reshape(-1, 1)
+#        h = h / l
 
         return h
 
