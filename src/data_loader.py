caa16aab346e3b7e946e909163246ca1a3a82830
KatoTetsuro <katotetsuro@team-lab.com>
2019-02-02 23:27:15 +0900

    なんの工夫もないcsvによるloaderを追加

--- src/data_loader.py
+++ src/data_loader.py
@@ -5,9 +5,10 @@ import torchvision
 import pandas as pd
 
 
-class LabeledImageDataset(data.Dataset):
+class WhaleLabeledImageDataset(data.Dataset):
     """csvファイルからデータセットを作る。
     train_with_id.csvの作り方はnotebooks/split_dataset.ipynbを参照
+    label=0のwhaleのバランスを調整する機能付き
     """
 
     def __init__(self, data_dir, transform=None):
@@ -57,6 +58,44 @@ class LabeledImageDataset(data.Dataset):
         self.new_whale_index += self.new_whale_size
         self.new_whale_index %= len(self.df_new_whale)
         self.df = df.reset_index(drop=True)
+
+    def __repr__(self):
+        fmt_str = 'Dataset ' + self.__class__.__name__ + '\n'
+        fmt_str += '    Number of datapoints: {}\n'.format(self.__len__())
+        return fmt_str
+
+
+class CsvLabeledImageDataset(data.Dataset):
+    """csvファイルからデータセットを作る。
+    train_with_id.csvの作り方はnotebooks/split_dataset.ipynbを参照
+    """
+
+    def __init__(self, csv_path, img_dir, transform=None):
+        self.df = pd.read_csv(csv_path)
+        self.image_dir = img_dir
+
+        if transform:
+            self.transform = transform
+        else:
+            self.transform = torchvision.transforms.ToTensor
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            Tensor image
+            int label
+        """
+        # todo このパースをもっと汎用的に作る？
+        img_file, _, label = self.df.iloc[index]
+        img = Image.open(join(self.image_dir, img_file)).convert('RGB')
+        img = self.transform(img)
+
+        return img, label
+
+    def __len__(self):
+        return len(self.df)
 
     def __repr__(self):
         fmt_str = 'Dataset ' + self.__class__.__name__ + '\n'
